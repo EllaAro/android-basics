01da39d4cd94800d08b060981aae74ca2240a8ed
Ella Aronov <ellaarono1994@gmail.com>
2021-06-11 21:44:03 +0300

    val vs var, switch case in kotlin

---
+++ HelloWorld/app/src/main/java/com/example/helloworld/KotlinBasics.kt
@@ -0,0 +1,15 @@
+package com.example.helloworld
+
+fun main() {
+    var myName = "Ella" //we create a variable. val can't be mutable , but var can be
+    print("Hello ${myName}\n")
+
+    val month = 3
+    when (month) {
+        in 3..5 -> println("Spring")
+        in 6..8 -> println("Summer")
+        in 9..11 -> println("Fall")
+        12, 1, 2 -> println("Hello Winter")
+        else -> println("Invalid month")
+    }
+}
--- HelloWorld/app/src/main/java/com/example/helloworld/MainActivity.kt
+++ HelloWorld/app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -11,7 +11,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        
+
         var timesClicked = 1
         // set on-click listener
         button.setOnClickListener {
