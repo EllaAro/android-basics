76d01c5006119be88a9d06c001c4d49f79cf1d4a
Ella Aronov <ellaarono1994@gmail.com>
2021-06-11 21:05:39 +0300

    adding some basic android project

---
+++ HelloWorld/app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.helloworld
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        
+        var timesClicked = 1
+        // set on-click listener
+        button.setOnClickListener {
+            textView.text = timesClicked.toString()
+            timesClicked++
+            Toast.makeText(this@MainActivity, "You clicked me.", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
