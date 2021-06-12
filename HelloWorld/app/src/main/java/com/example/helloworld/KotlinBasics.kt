3d99fd994b4d5afea2f2edf7171dc8110111c102
Ella Aronov <ellaarono1994@gmail.com>
2021-06-12 21:31:49 +0300

    classes in kotlin, data classes, inheritence

--- HelloWorld/app/src/main/java/com/example/helloworld/KotlinBasics.kt
+++ HelloWorld/app/src/main/java/com/example/helloworld/KotlinBasics.kt
@@ -29,8 +29,25 @@ fun main() {
     println(myCar.myBrand)
      */
 
+    /*
     val user1 = User(1,"Ella")
     println(user1.name)
+    val user2 = User(1,"Ella")
+
+    println(user1.equals(user2))
+
+    val updatedUser = user1.copy(name="Michael")
+    println(user1)
+    println(updatedUser)
+
+    println(user1.component1()) // prints id
+    println(user1.component2()) // prints name
+
+    // de-construction
+    val (id, name ) = updatedUser
+
+     */
+
 
 }
 
@@ -80,4 +97,26 @@ class Car() {
 
 data class User(val id: Long, val name: String) {
 
+}
+
+/*
+* The class that inherits the features of another
+* class is called sub class or child class
+* or derived class, the class whose features are
+* inherited is called the super class or parent class or base class
+*
+* Kotlin classes are final by default therefore we need to add 'open' at the beginning of the class
+* you cannot inherit from a sealed class as well
+* */
+open class Vehicle {
+    // properties
+    // methods
+}
+
+open class Bus(name: String) : Vehicle() {
+
+}
+
+class ElectricBus(name: String) : Bus(name) {
+    
 }
